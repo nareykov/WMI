a13f6a67429cad13444f738ca513ae2d272ee7c7
nareykov <nareykov@mail.ru>
2017-10-30 16:33:22 +0300

    Добавьте файлы проекта.

---
+++ wmi/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Management;
+using System;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace WMI
+{
+    class Program
+    {       
+        public static void Main(string[] args)
+        {
+            Devices devices = new PCIDevices();
+            foreach (var device in devices.findDevices())
+            {
+                Console.WriteLine("VendorID: {0} , DeviceID {1}", device.getVendorID(), device.getDeviceID());
+            }
+            Console.ReadKey();
+        }
+    }
+}
